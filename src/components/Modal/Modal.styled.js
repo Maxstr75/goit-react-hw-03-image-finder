e3f23eec2bb861f1ab25d17f95a397d8525e3413
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-17 17:04:57 +0300

    update stayled

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -16,5 +16,5 @@ export const Overlay = styled.div`
 export const ModalStyled = styled.div`
   max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
-  background-color: #fffff;
+  background-color: #fff;
 `;
