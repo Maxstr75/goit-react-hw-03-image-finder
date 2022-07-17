3cdaa82c1865660a6e0b23172702548a6a674326
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-07-17 16:47:03 +0300

    update

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,45 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Overlay, ModalStyled } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    onClose: PropTypes.func,
+    children: PropTypes.node.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalStyled>{this.props.children}</ModalStyled>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,64 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { MdImageSearch } from 'react-icons/md';
+import {
+  SearchHeader,
+  SearchForm,
+  SearchFormInput,
+  SearchButton,
+} from './SearchBar.styled';
+
+class SearchBar extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  };
+
+  state = {
+    query: '',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.currentTarget.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.query.trim() === '') {
+      toast.warn('Please specify your query!');
+      return;
+    }
+    this.props.onSubmit(this.state.query);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ query: '' });
+  };
+
+  render() {
+    const { query } = this.state;
+
+    return (
+      <SearchHeader>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <SearchButton type="submit">
+            <MdImageSearch style={{ width: 30, height: 30 }} />
+          </SearchButton>
+
+          <SearchFormInput
+            type="text"
+            name="query"
+            value={query}
+            onChange={this.handleChange}
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </SearchForm>
+      </SearchHeader>
+    );
+  }
+}
+
+export default SearchBar;
